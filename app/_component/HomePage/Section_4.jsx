5a7b4adf64cbad5e49d15ca9a432c5e16fb56ebe
4xeoz <iyadchirifi940@gmail.com>
2025-02-14 18:09:38 +0000

    Section_4 fixed

--- app/_component/HomePage/Section_4.jsx
+++ app/_component/HomePage/Section_4.jsx
@@ -26,7 +26,7 @@ const Section_4 = () => {
       <div className="flex max-w-5xl flex-col lg:flex-row bg-[rgb(235,235,235)] p-8 md:p-16 gap-8 md:gap-16 rounded-3xl shadow-lg border-2 border-white">
             <div className="w-full lg:w-1/3 max-w-[280px] mx-auto relative aspect-[9/16]">
               <Image
-                src="/AppScreenshot.png"
+                src="/AppScreenShot.png"
                 alt="App Screenshot"
                 fill
                 className="rounded-3xl object-cover"
@@ -57,7 +57,7 @@ const Section_4 = () => {
             </div>
             <div className="w-full lg:w-1/3 max-w-[280px] mx-auto relative aspect-[9/16]">
               <Image
-                src="/NotificationScreenshot.png"
+                src="/NotificationScreenShot.png"
                 alt="Notification Screenshot"
                 fill
                 className="rounded-3xl object-cover"
@@ -75,7 +75,7 @@ const Section_4 = () => {
       <div className="flex max-w-5xl flex-row bg-[rgb(235,235,235)] rounded-3xl shadow-lg border-2 border-white overflow-hidden">
             <div className="w-1/2 relative aspect-[9/16]">
               <Image
-                src="/AppScreenshot.png"
+                src="/AppScreenShot.png"
                 alt="App Screenshot"
                 fill
                 className="object-cover object-left"
@@ -95,7 +95,7 @@ const Section_4 = () => {
           <div className="flex max-w-5xl flex-row bg-[rgb(235,235,235)] rounded-3xl shadow-lg border-2 border-white overflow-hidden">
             <div className="w-1/2 relative aspect-[9/16]">
               <Image
-                src="/NotificationScreenshot.png"
+                src="/NotificationScreenShot.png"
                 alt="Notification Screenshot"
                 fill
                 className="object-cover object-left"
