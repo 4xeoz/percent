4759075e4d49288233ed563d82f2f8ffdfdf7a17
4xeoz <iyadchirifi940@gmail.com>
2025-02-14 17:59:22 +0000

    Section_4 fixed

--- app/_component/HomePage/Section_4.jsx
+++ app/_component/HomePage/Section_4.jsx
@@ -20,11 +20,10 @@ const Section_4 = () => {
     };
   }, []);
 
-  return (
+  if ( isLargeScreen ) {  return ( 
     <div className="my-24 md:my-48 flex flex-col gap-24 md:gap-48 min-h-screen items-center justify-center p-4">
       <GlidAnimation>
-        {isLargeScreen ? (
-          <div className="flex max-w-5xl flex-col lg:flex-row bg-[rgb(235,235,235)] p-8 md:p-16 gap-8 md:gap-16 rounded-3xl shadow-lg border-2 border-white">
+      <div className="flex max-w-5xl flex-col lg:flex-row bg-[rgb(235,235,235)] p-8 md:p-16 gap-8 md:gap-16 rounded-3xl shadow-lg border-2 border-white">
             <div className="w-full lg:w-1/3 max-w-[280px] mx-auto relative aspect-[9/16]">
               <Image
                 src="/AppScreenshot.png"
@@ -43,8 +42,37 @@ const Section_4 = () => {
               </div>
             </div>
           </div>
-        ) : (
-          <div className="flex max-w-5xl flex-row bg-[rgb(235,235,235)] rounded-3xl shadow-lg border-2 border-white overflow-hidden">
+
+      </GlidAnimation>
+      <GlidAnimation>
+      <div className="flex max-w-5xl flex-col lg:flex-row bg-[rgb(235,235,235)] p-8 md:p-16 gap-8 md:gap-16 rounded-3xl shadow-lg border-2 border-white">
+            <div className="flex flex-col justify-center space-y-4 md:space-y-6 lg:w-2/3 lg:pr-12 xl:pr-20">
+              <h1 className="text-3xl md:text-4xl font-bold tracking-tight">A Simple Reminder, Every Midnight</h1>
+              <div className="space-y-2 md:space-y-4 text-muted-foreground text-sm md:text-base">
+                <p>
+                  At 12:00 AM, Percent sends a notification showing how much of the month has passed—helping you stay
+                  aware and make the most of what&apos;s left.
+                </p>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/3 max-w-[280px] mx-auto relative aspect-[9/16]">
+              <Image
+                src="/NotificationScreenshot.png"
+                alt="Notification Screenshot"
+                fill
+                className="rounded-3xl object-cover"
+                sizes="(max-width: 1024px) 100vw, 33vw"
+              />
+            </div>
+          </div>
+      </GlidAnimation>
+    </div>
+  
+
+  )} else { return (
+    <div className="my-24 md:my-48 flex flex-col gap-24 md:gap-48 min-h-screen items-center justify-center p-4">
+      <GlidAnimation>
+      <div className="flex max-w-5xl flex-row bg-[rgb(235,235,235)] rounded-3xl shadow-lg border-2 border-white overflow-hidden">
             <div className="w-1/2 relative aspect-[9/16]">
               <Image
                 src="/AppScreenshot.png"
@@ -63,36 +91,11 @@ const Section_4 = () => {
               </div>
             </div>
           </div>
-        )}
-      </GlidAnimation>
 
-      <GlidAnimation>
-        {isLargeScreen ? (
-          <div className="flex max-w-5xl flex-col lg:flex-row bg-[rgb(235,235,235)] p-8 md:p-16 gap-8 md:gap-16 rounded-3xl shadow-lg border-2 border-white">
-            <div className="flex flex-col justify-center space-y-4 md:space-y-6 lg:w-2/3 lg:pr-12 xl:pr-20">
-              <h1 className="text-3xl md:text-4xl font-bold tracking-tight">A Simple Reminder, Every Midnight</h1>
-              <div className="space-y-2 md:space-y-4 text-muted-foreground text-sm md:text-base">
-                <p>
-                  At 12:00 AM, Percent sends a notification showing how much of the month has passed—helping you stay
-                  aware and make the most of what&apos;s left.
-                </p>
-              </div>
-            </div>
-            <div className="w-full lg:w-1/3 max-w-[280px] mx-auto relative aspect-[9/16]">
-              <Image
-                src="/WidgetScreenshot.png"
-                alt="Notification Screenshot"
-                fill
-                className="rounded-3xl object-cover"
-                sizes="(max-width: 1024px) 100vw, 33vw"
-              />
-            </div>
-          </div>
-        ) : (
           <div className="flex max-w-5xl flex-row bg-[rgb(235,235,235)] rounded-3xl shadow-lg border-2 border-white overflow-hidden">
             <div className="w-1/2 relative aspect-[9/16]">
               <Image
-                src="/WidgetScreenshot.png"
+                src="/NotificationScreenshot.png"
                 alt="Notification Screenshot"
                 fill
                 className="object-cover object-left"
@@ -109,11 +112,13 @@ const Section_4 = () => {
               </div>
             </div>
           </div>
-        )}
+        
+
+
       </GlidAnimation>
     </div>
-  )
-}
 
+  ) }
+}
 export default Section_4
 
