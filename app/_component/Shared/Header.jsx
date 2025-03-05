ad3d58a01d477ea6f75164f1afa60743f2ce78a4
4xeoz <iyadchirifi940@gmail.com>
2025-03-05 02:05:29 +0000

    downlink updated, support eamil updated

--- app/_component/Shared/Header.jsx
+++ app/_component/Shared/Header.jsx
@@ -3,32 +3,29 @@ import GlidDownAnimation from '@/components/GlidDownAnimation'
 
 const Header = () => {
 return (
-    <header className="w-full py-6 px-4 sm:px-6 lg:px-8">
-      <GlidDownAnimation>
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <div className="flex items-center space-x-4">
-          <a href="/"> 
-            <img src="/LogoHeader.png" alt="Logo" className=" w-10 h-10 md:w-16 md:h-16" />
-          </a>
-        </div>
+  <header className="w-full py-6 px-4 sm:px-6 lg:px-8">
+    <GlidDownAnimation>
+    <div className="max-w-7xl mx-auto flex items-center justify-between">
+    <div className="flex items-center space-x-4">
+      <a href="/"> 
+      <img src="/LogoHeader.png" alt="Logo" className=" w-10 h-10 md:w-16 md:h-16" />
+      </a>
+    </div>
 
-        <div className="flex items-center space-x-6">
-          <nav className=" flex space-x-6">
-            <a href="/policy" className="md:text-base text-xs font-medium text-gray-700 hover:text-gray-900">
-              <p>Policy</p>
-            </a>
+    <div className="flex items-center space-x-6">
+      <nav className=" flex space-x-6">
+      <a href="/policy" className="md:text-base text-xs font-medium text-gray-700 hover:text-gray-900">
+        <p>Policy</p>
+      </a>
+      </nav>
 
-          </nav>
-
-          <button
-            className="rounded-full border-2 border-black bg-[#D32020] text-black hover:bg-red-600 transition-colors px-8 md:px-16 py-3 md:py-5 text-base md:text-lg font-semibold"
-          >
-            <p>Download</p>
-          </button>
-        </div>
-      </div>
-      </GlidDownAnimation>
-    </header>
+      <a href="https://apps.apple.com/gb/app/percent-calendar/id6742252217" className="rounded-full border-2 border-black bg-[#D32020] text-black hover:bg-red-600 transition-colors px-8 md:px-16 py-3 md:py-5 text-base md:text-lg font-semibold">
+      <p>Download</p>
+      </a>
+    </div>
+    </div>
+    </GlidDownAnimation>
+  </header>
 )
 }
 
